96bdb444b03158c84b5b6eac25277482e0945054
mahadi99900 <151855877+mahadi99900@users.noreply.github.com>
2025-09-19 17:28:20 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,10 @@
 module.exports = {
-    // --- LOGIN CREDENTIALS ---
-    // The bot will use these credentials only the first time or when the session expires.
-    email: "+8801874397626",
-    password: "hsuxhsizuwbx6318",
-    
-    // --- BOT ADMIN ---
-    // Your own Facebook User ID for admin commands.
-    adminID: "100052951819398",
+    // --- আপনার Raw Cookie String এখানে পেস্ট করুন ---
+    rawCookie: "Datr=qDzNaKF6o6HULPeBDN4n1_T5; sb=qDzNaPbN1qFBu1EVkI2OJiPE; m_pixel_ratio=2; wd=360x806; c_user=61578564513586; fr=0pIjqR9otCFr9ElQL.AWeGR4iS9X5YGZbqbVjObGfRhgZjvRwRuTxNsZHg79XAY83X7Cs.BozTyo..AAA.0.0.BozTzP.AWdY4OA8qBZ8VeuSJLieOB8NvbQ; xs=35%3A49NiJ-hv0ddIHg%3A2%3A1758280912%3A-1%3A-1",
 
-    // --- BOT SETTINGS ---
-    // The prefix for bot commands.
+    // --- বট অ্যাডমিন ---
+    adminID: "YOUR_FACEBOOK_USER_ID_HERE",
+
+    // --- বট সেটিংস ---
     prefix: "/",
 };
