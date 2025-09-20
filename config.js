494d79b8d01d185dee363640e5e68769e3192117
mahadi99900 <151855877+mahadi99900@users.noreply.github.com>
2025-09-20 14:20:58 +0600

    Update config.js

--- config.js
+++ config.js
@@ -2,143 +2,127 @@ module.exports = {
     // --- ফেসবুক কুকিজ ---
     // আপনার নতুন কুকিজ এখানে বসানো হয়েছে
     appstate: [
-    {
-        "name": "datr",
-        "value": "0lPOaLqP8-KxHCEoNlcWALia",
-        "domain": ".facebook.com",
-        "hostOnly": false,
-        "path": "/",
-        "secure": true,
-        "httpOnly": true,
-        "sameSite": "no_restriction",
-        "session": false,
-        "firstPartyDomain": "",
-        "partitionKey": null,
-        "expirationDate": 1792912337.497,
-        "storeId": null
-    },
-    {
-        "name": "fr",
-        "value": "1hgkjshhL8JB9hH1p.AWeM_ju7SAL9Qu1jRf2dOUNzfoXBDzg-59lzueMmhQlmWrqO7RQ.BozlT-..AAA.0.0.BozlUe.AWdrcnGioi7s1vyGcc279-S3LLA",
-        "domain": ".facebook.com",
-        "hostOnly": false,
-        "path": "/",
-        "secure": true,
-        "httpOnly": true,
-        "sameSite": "no_restriction",
-        "session": false,
-        "firstPartyDomain": "",
-        "partitionKey": null,
-        "expirationDate": 1766128668.78,
-        "storeId": null
-    },
-    {
-        "name": "xs",
-        "value": "28%3AChOlEXBit_pkaA%3A2%3A1758352613%3A-1%3A-1%3A%3AAcWLEzY7jWr4egGoeZYf-0XZcv_pR0t1JkU9sWjH6A",
-        "domain": ".facebook.com",
-        "hostOnly": false,
-        "path": "/",
-        "secure": true,
-        "httpOnly": true,
-        "sameSite": "no_restriction",
-        "session": false,
-        "firstPartyDomain": "",
-        "partitionKey": null,
-        "expirationDate": 1789888637.459,
-        "storeId": null
-    },
-    {
-        "name": "m_pixel_ratio",
-        "value": "1",
-        "domain": ".facebook.com",
-        "hostOnly": false,
-        "path": "/",
-        "secure": true,
-        "httpOnly": false,
-        "sameSite": null,
-        "session": true,
-        "firstPartyDomain": "",
-        "partitionKey": null,
-        "storeId": null
-    },
-    {
-        "name": "c_user",
-        "value": "61581294953813",
-        "domain": ".facebook.com",
-        "hostOnly": false,
-        "path": "/",
-        "secure": true,
-        "httpOnly": false,
-        "sameSite": "no_restriction",
-        "session": false,
-        "firstPartyDomain": "",
-        "partitionKey": null,
-        "expirationDate": 1789888637.459,
-        "storeId": null
-    },
-    {
-        "name": "presence",
-        "value": "C%7B%22t3%22%3A%5B%5D%2C%22utc3%22%3A1758352676249%2C%22v%22%3A1%7D",
-        "domain": ".facebook.com",
-        "hostOnly": false,
-        "path": "/",
-        "secure": true,
-        "httpOnly": false,
-        "sameSite": null,
-        "session": true,
-        "firstPartyDomain": "",
-        "partitionKey": null,
-        "storeId": null
-    },
-    {
-        "name": "dpr",
-        "value": "2",
-        "domain": ".facebook.com",
-        "hostOnly": false,
-        "path": "/",
-        "secure": true,
-        "httpOnly": false,
-        "sameSite": "no_restriction",
-        "session": false,
-        "firstPartyDomain": "",
-        "partitionKey": null,
-        "expirationDate": 1758957161,
-        "storeId": null
-    },
-    {
-        "name": "sb",
-        "value": "0lPOaMLklq_OtKx_K8ZqOZsF",
-        "domain": ".facebook.com",
-        "hostOnly": false,
-        "path": "/",
-        "secure": true,
-        "httpOnly": true,
-        "sameSite": "no_restriction",
-        "session": false,
-        "firstPartyDomain": "",
-        "partitionKey": null,
-        "expirationDate": 1792912613.578,
-        "storeId": null
-    },
-    {
-        "name": "wd",
-        "value": "980x1942",
-        "domain": ".facebook.com",
-        "hostOnly": false,
-        "path": "/",
-        "secure": true,
-        "httpOnly": false,
-        "sameSite": "lax",
-        "session": false,
-        "firstPartyDomain": "",
-        "partitionKey": null,
-        "expirationDate": 1758957475,
-        "storeId": null
-    }
-],
+        {
+            "key": "dbln",
+            "value": "%7B%2261581294953813%22%3A%22mdIPEzQt%22%7D",
+            "domain": "facebook.com",
+            "path": "/login/device-based/",
+            "hostOnly": false,
+            "creation": "2025-09-20T08:18:18.098Z",
+            "lastAccessed": "2025-09-20T08:18:18.100Z"
+        },
+        {
+            "key": "datr",
+            "value": "BmPOaEYVvuVDavdo0DpiobSf",
+            "domain": "facebook.com",
+            "path": "/",
+            "hostOnly": false,
+            "creation": "2025-09-20T08:18:18.100Z",
+            "lastAccessed": "2025-09-20T08:18:18.100Z"
+        },
+        {
+            "key": "sb",
+            "value": "BmPOaNfQhnsLjuaMPaq9vzLQ",
+            "domain": "facebook.com",
+            "path": "/",
+            "hostOnly": false,
+            "creation": "2025-09-20T08:18:18.100Z",
+            "lastAccessed": "2025-09-20T08:18:18.100Z"
+        },
+        {
+            "key": "m_pixel_ratio",
+            "value": "2",
+            "domain": "facebook.com",
+            "path": "/",
+            "hostOnly": false,
+            "creation": "2025-09-20T08:18:18.100Z",
+            "lastAccessed": "2025-09-20T08:18:18.100Z"
+        },
+        {
+            "key": "wd",
+            "value": "360x806",
+            "domain": "facebook.com",
+            "path": "/",
+            "hostOnly": false,
+            "creation": "2025-09-20T08:18:18.100Z",
+            "lastAccessed": "2025-09-20T08:18:18.100Z"
+        },
+        {
+            "key": "c_user",
+            "value": "61581294953813",
+            "domain": "facebook.com",
+            "path": "/",
+            "hostOnly": false,
+            "creation": "2025-09-20T08:18:18.100Z",
+            "lastAccessed": "2025-09-20T08:18:18.100Z"
+        },
+        {
+            "key": "xs",
+            "value": "14%3Al6hwErCop_74vg%3A2%3A1758356261%3A-1%3A-1",
+            "domain": "facebook.com",
+            "path": "/",
+            "hostOnly": false,
+            "creation": "2025-09-20T08:18:18.100Z",
+            "lastAccessed": "2025-09-20T08:18:18.100Z"
+        },
+        {
+            "key": "fr",
+            "value": "0XFuFQeP5tHp6NU2E.AWdb1gtF2qHConpH5YWD_0IXHQZrX5eHTHJ_TVOJ1s7w4vPJhhk.BozmMG..AAA.0.0.BozmMu.AWdVxucd6ebwknmSslGWJDjo-D8",
+            "domain": "facebook.com",
+            "path": "/",
+            "hostOnly": false,
+            "creation": "2025-09-20T08:18:18.100Z",
+            "lastAccessed": "2025-09-20T08:18:18.100Z"
+        },
+        {
+            "key": "locale",
+            "value": "bn_IN",
+            "domain": "facebook.com",
+            "path": "/",
+            "hostOnly": false,
+            "creation": "2025-09-20T08:18:18.100Z",
+            "lastAccessed": "2025-09-20T08:18:18.100Z"
+        },
+        {
+            "key": "pas",
+            "value": "61581294953813%3AxopOg0NCMY",
+            "domain": "facebook.com",
+            "path": "/",
+            "hostOnly": false,
+            "creation": "2025-09-20T08:18:18.100Z",
+            "lastAccessed": "2025-09-20T08:18:18.100Z"
+        },
+        {
+            "key": "wl_cbv",
+            "value": "v2%3Bclient_version%3A2928%3Btimestamp%3A1758356270",
+            "domain": "facebook.com",
+            "path": "/",
+            "hostOnly": false,
+            "creation": "2025-09-20T08:18:18.100Z",
+            "lastAccessed": "2025-09-20T08:18:18.100Z"
+        },
+        {
+            "key": "fbl_st",
+            "value": "100632677%3BT%3A29305937",
+            "domain": "facebook.com",
+            "path": "/",
+            "hostOnly": false,
+            "creation": "2025-09-20T08:18:18.100Z",
+            "lastAccessed": "2025-09-20T08:18:18.100Z"
+        },
+        {
+            "key": "vpd",
+            "value": "v1%3B714x360x2",
+            "domain": "facebook.com",
+            "path": "/",
+            "hostOnly": false,
+            "creation": "2025-09-20T08:18:18.100Z",
+            "lastAccessed": "2025-09-20T08:18:18.100Z"
+        }
+    ],
 
     // --- বট অ্যাডমিন ---
-    // এখানে আপনার নতুন ফেসবুক আইডির ইউজার আইডি বসান (ঐচ্ছিক)
+    // এখানে আপনার ফেসবুক আইডির ইউজার আইডি বসান (ঐচ্ছিক)
     adminID: "YOUR_FACEBOOK_USER_ID_HERE",
 
     // --- বট সেটিংস ---
